7b012850919ea2258093fccee22db4e6406dbdef
usha-12 <usha12ahirwar@gmail.com>
2022-10-08 23:45:07 +0530

    Print Name Using Command Line

---
+++ src/Bridgelabz/Com/Day1practice/GetNameUsingCommandLine.java
@@ -0,0 +1,8 @@
+package Bridgelabz.Com.Day1practice;
+
+public class GetNameUsingCommandLine {
+    public static void main(String[] args) {
+        System.out.println("My Name is "+ args[0]+" "+args[1]);
+
+    }
+}
