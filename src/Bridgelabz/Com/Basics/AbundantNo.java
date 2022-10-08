18ba75e02a51978e990cdbc27ad1d6b7b500a7ca
usha-12 <usha12ahirwar@gmail.com>
2022-10-08 21:38:58 +0530

    Abundant Number program

--- src/Bridgelabz/Com/Basics/AbundantNo.java
+++ src/Bridgelabz/Com/Basics/AbundantNo.java
@@ -1,2 +1,21 @@
-package Bridgelabz.Com.Basics;public class AbundantNo {
+package Bridgelabz.Com.Basics;
+
+public class AbundantNo {
+    public static void checkAbundantNumber(int number){
+        int sum = 0;
+        for (int i = 1; i<number; i++){
+            if (number % i == 0)
+                sum = sum+i;
+        }
+        if (sum>number){
+            System.out.println("is an Abundant Number :"+number);
+            System.out.println("The Abundance is :"+(sum-number));
+        }else
+            System.out.println("is not an Abundant Number :"+number);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Abundant Number program");
+        checkAbundantNumber(12);
+
+    }
 }
