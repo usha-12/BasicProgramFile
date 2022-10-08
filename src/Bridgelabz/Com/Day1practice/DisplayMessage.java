5f00b4b75aa72bd6ca41d590e10f020f709441b4
usha-12 <usha12ahirwar@gmail.com>
2022-10-08 23:43:24 +0530

    Print Message

---
+++ src/Bridgelabz/Com/Day1practice/DisplayMessage.java
@@ -0,0 +1,9 @@
+package Bridgelabz.Com.Day1practice;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to java program!");
+    }
+}
+
