c9ba1e0a516cb2560fd5f33880de07b9044b228a
usha-12 <usha12ahirwar@gmail.com>
2022-10-08 22:30:49 +0530

    Greatest of Two Number

---
+++ src/Bridgelabz/Com/Basics/GreatestOf2Number.java
@@ -0,0 +1,16 @@
+package Bridgelabz.Com.Basics;
+
+public class GreatestOf2Number {
+    public static void checkGreatestOfTwoNumbers(int num1, int num2){
+        if(num1>num2)
+            System.out.println("Number is Greater : "+num1);
+        else
+            System.out.println("Number is Greater : "+num2);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Greatest of Two Numbers are :");
+        checkGreatestOfTwoNumbers(20, 30);
+    }
+}
+//Write a program to find the largest of two numbers..
+
